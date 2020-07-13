8244e7218a00c3af81fc87b51b4a3050100af109
Leroy Haimoff <leroyblergh@gmail.com>
2020-07-13 21:46:43 +0300

    Updates

---
+++ precache-manifest.f73bff23b25fca960d1647762767ac50.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf61146e33fd5672828610b23ecec317",
+    "url": "/shell-game/index.html"
+  },
+  {
+    "revision": "ae3eb1af1882b6831fc5",
+    "url": "/shell-game/static/css/main.eec614f1.chunk.css"
+  },
+  {
+    "revision": "24b6e2345b74969a14db",
+    "url": "/shell-game/static/js/2.65665174.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/shell-game/static/js/2.65665174.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ae3eb1af1882b6831fc5",
+    "url": "/shell-game/static/js/main.e2a50910.chunk.js"
+  },
+  {
+    "revision": "a71c8a36299bd9ab9f68",
+    "url": "/shell-game/static/js/runtime-main.d669565f.js"
+  },
+  {
+    "revision": "2c129c9e0e33e702bc73170462be4733",
+    "url": "/shell-game/static/media/back.2c129c9e.png"
+  },
+  {
+    "revision": "f5bf5b5e07dee89468d29e4245bd1a2b",
+    "url": "/shell-game/static/media/background.f5bf5b5e.jpg"
+  },
+  {
+    "revision": "d2fdbd06e114e00eba65432e80dda674",
+    "url": "/shell-game/static/media/balloons.d2fdbd06.gif"
+  },
+  {
+    "revision": "7f57a96673574a059d7d8262d6af0525",
+    "url": "/shell-game/static/media/board.7f57a966.jpg"
+  },
+  {
+    "revision": "e4d2c1d0da356797359acd9270bcdd77",
+    "url": "/shell-game/static/media/confetti.e4d2c1d0.gif"
+  },
+  {
+    "revision": "9e556d6377e08fd0bd78ec8b8126a9bb",
+    "url": "/shell-game/static/media/players.9e556d63.png"
+  },
+  {
+    "revision": "18d108fc3250fca72ac3cb432393d436",
+    "url": "/shell-game/static/media/star.18d108fc.png"
+  }
+]);
